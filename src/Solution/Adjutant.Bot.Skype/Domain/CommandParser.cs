767fbcc39c426157988137898b1d43d8a3704d4e
Leshk <aliaksei.kasmachou@gmail.com>
2017-04-08 15:18:21 +0300

    Command Line connect model

--- src/Solution/Adjutant.Bot.Skype/Domain/CommandParser.cs
+++ src/Solution/Adjutant.Bot.Skype/Domain/CommandParser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Text.RegularExpressions;
+using Adjutant.Bot.Skype.Models;
 
 namespace Adjutant.Bot.Skype.Domain
 {
@@ -10,11 +11,9 @@ namespace Adjutant.Bot.Skype.Domain
 	{
 		//commad
 		public string command;
-		public string owner;
-		public string repository;
-		public string alias;
+		public string command_connect = "connect";
 
-		public int Parse(string commandsLine)
+		public BaseEntity Parse(string commandsLine)
 		{
 			string pattern = " ";
 			String[] commandArgs = Regex.Split(commandsLine, pattern);
@@ -23,11 +22,9 @@ namespace Adjutant.Bot.Skype.Domain
 			switch (command.ToLower())
 			{
 				case "connect":
-					GetArgumentsConnect(commandArgs);
-					break;
-
+					return CreateCommandModel(commandArgs);
 				case "status":
-
+					return CreateStatusModel(commandArgs);
 					break;
 
 				case "pull_request":
@@ -35,10 +32,18 @@ namespace Adjutant.Bot.Skype.Domain
 					break;
 			}
 
-			return default(int);
+			return default(BaseEntity);
 		}
-		string GetArgumentsConnect(string[] commandArgs)
+
+		public BaseEntity CreateCommandModel(string[] commandArgs)
 		{
+			var result = new ConnectRequetsModel();
+			string repository = null;
+			string owner = null;
+			string alias = null;
+			string repositoryName = null;
+
+			string gitHost = ".git";
 			bool noName = false;
 			string tempAlise = "";
 			if (commandArgs.Length < 3)
@@ -46,15 +51,16 @@ namespace Adjutant.Bot.Skype.Domain
 
 			foreach (string arg in commandArgs)
 			{
-				if (arg.Contains("http") && arg.Contains(".git"))
+				if (arg.Contains("http") && arg.Contains(gitHost))
 				{
 					repository = arg;
 					String[] elementsUrl = Regex.Split(arg, "/");
 					owner = elementsUrl[3];
 					tempAlise = elementsUrl[4];
+					repositoryName = elementsUrl[4].Replace(gitHost, "");
 				} else
 				{
-					if (!arg.Contains("connect"))
+					if (!arg.Contains(command_connect))
 						alias = arg;
 				}
 			}
@@ -62,7 +68,18 @@ namespace Adjutant.Bot.Skype.Domain
 			if (noName)
 				alias = tempAlise;
 
-			return "";
+			result.Alias = alias;
+			result.Owner = owner;
+			result.RepositoryUrl = repository;
+			result.RepositoryName = repositoryName;
+
+			return result;
 		}
-	}
+
+		public BaseEntity CreateStatusModel(string[] commandArgs)
+		{
+			var result = new ConnectRequetsModel();
+
+			return result;
+		}
 }
---
+++ src/Solution/Adjutant.Bot.Skype/Models/ConnectRequetsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Adjutant.Bot.Skype.Models
+{
+	public class ConnectRequetsModel : BaseEntity
+	{
+		public long ClientId { get; set; }
+
+		public string RepositoryUrl { get; set; }
+
+		public string Alias { get; set; }
+
+		public string Owner { get; set; }
+
+		public string RepositoryName { get; set; }
+	}
+}
