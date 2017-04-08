fc3655be082c4aa91bb83b3a2aa58e8752e4946b
Alexander Belenkov <belenkov.alexander@gmail.com>
2017-04-08 13:12:58 +0300

    add api projects

---
+++ src/Solution/Adjutant.Api/Controllers/BotController.cs
@@ -0,0 +1,21 @@
+﻿using Adjutant.Common.Models;
+using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Adjutant.Api.Controllers
+{
+    public class BotController : ApiController
+    {
+        [SwaggerOperation("Create")]
+        [SwaggerResponse(HttpStatusCode.Created)]
+        public void Post([FromBody]ConnectPostRequestModel model)
+        {
+
+        }
+    }
+}
---
+++ src/Solution/Adjutant.Common/Models/ConnectPostRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Adjutant.Common.Models
+{
+    public class ConnectPostRequestModel
+    {
+        public string Owner { get; set; }
+
+        public string RepositoryName { get; set; }
+
+        public string Alias { get; set; }
+    }
+}
