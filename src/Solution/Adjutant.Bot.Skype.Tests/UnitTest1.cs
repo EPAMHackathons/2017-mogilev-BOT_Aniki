eb5bc59540d32340d02b9b5b3adbea5d134af9fe
Alexander Belenkov <belenkov.alexander@gmail.com>
2017-04-08 10:12:37 +0300

    Create bot project

---
+++ src/Solution/Adjutant.Bot.Skype.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Adjutant.Bot.Skype.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
