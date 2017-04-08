9d4b233dea09f955276de18af28ee46d661e21c0
Leshk <aliaksei.kasmachou@gmail.com>
2017-04-08 17:28:18 +0300

    fix merge

--- src/Solution/Adjutant.Bot.Skype/Models/ConnectRequetsModel.cs
+++ src/Solution/Adjutant.Bot.Skype/Models/ConnectRequetsModel.cs
@@ -1,15 +1,22 @@
-﻿namespace Adjutant.Bot.Skype.Models
+﻿using System.Collections.Generic;
+using System.Security.Policy;
+
+namespace Adjutant.Bot.Skype.Models
 {
-    public class ConnectRequetsModel : BaseEntity
-    {
-        public long ClientId { get; set; }
+	public class ConnectRequetsModel : BaseEntity
+	{
+		public long ClientId { get; set; }
 
-        public string RepositoryUrl { get; set; }
+		public Url RepositoryUrl { get; set; }
 
-        public string Alias { get; set; }
+		public string Alias { get; set; }
 
-        public string Owner { get; set; }
+		public string Owner { get; set; }
 
-        public string RepositoryName { get; set; }
-    }
+		public string RepositoryName { get; set; }
+
+		public int TimePeriod { get; set; }
+
+		public IList<string> Users { get; set; }
+	}
 }
