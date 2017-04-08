787e5c2723e9db768edf782a8450a33455b941b6
Leshk <aliaksei.kasmachou@gmail.com>
2017-04-08 14:46:36 +0300

    Command line Parser impl connect command

---
+++ src/Solution/Adjutant.Bot.Skype.Tests/CommandParserTests.cs
@@ -0,0 +1,21 @@
+﻿using Adjutant.Bot.Skype.Domain;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adjutant.Bot.Skype.Tests
+{
+	[TestFixture]
+	public class CommandParserTests
+	{
+		[Test]
+		public void GetIssuesTest()
+		{
+			var instance = new CommandParser();
+			var result = instance.Parse("connect https://github.com/EPAMHackathons/2017-mogilev-BOT_Aniki.git BotAniki");
+		} 
+	}
+}
---
+++ src/Solution/Adjutant.Bot.Skype/Domain/CommandParser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace Adjutant.Bot.Skype.Domain
+{
+	public class CommandParser
+	{
+		//commad
+		public string command;
+		public string owner;
+		public string repository;
+		public string alias;
+
+		public int Parse(string commandsLine)
+		{
+			string pattern = " ";
+			String[] commandArgs = Regex.Split(commandsLine, pattern);
+			command = commandArgs[0];
+
+			switch (command.ToLower())
+			{
+				case "connect":
+					GetArgumentsConnect(commandArgs);
+					break;
+
+				case "status":
+
+					break;
+
+				case "pull_request":
+
+					break;
+			}
+
+			return default(int);
+		}
+		string GetArgumentsConnect(string[] commandArgs)
+		{
+			bool noName = false;
+			string tempAlise = "";
+			if (commandArgs.Length < 3)
+				noName = !noName;
+
+			foreach (string arg in commandArgs)
+			{
+				if (arg.Contains("http") && arg.Contains(".git"))
+				{
+					repository = arg;
+					String[] elementsUrl = Regex.Split(arg, "/");
+					owner = elementsUrl[3];
+					tempAlise = elementsUrl[4];
+				} else
+				{
+					if (!arg.Contains("connect"))
+						alias = arg;
+				}
+			}
+
+			if (noName)
+				alias = tempAlise;
+
+			return "";
+		}
+	}
+}
